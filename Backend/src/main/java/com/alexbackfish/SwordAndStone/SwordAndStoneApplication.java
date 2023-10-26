2110b2fe46d5e34c2c7c3abf081c478995ce5f33
alex <backfish.alexander@gmail.com>
2023-10-26 01:40:58 -0400

    Backend added

---
+++ Backend/src/main/java/com/alexbackfish/SwordAndStone/SwordAndStoneApplication.java
@@ -0,0 +1,13 @@
+package com.alexbackfish.SwordAndStone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwordAndStoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwordAndStoneApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/alexbackfish/SwordAndStone/SwordAndStoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexbackfish.SwordAndStone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwordAndStoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
