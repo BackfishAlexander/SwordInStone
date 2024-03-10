d2a2e8505cf15c1def717c8b1685825950877fc7
alex <backfish.alexander@gmail.com>
2024-03-09 23:21:07 -0500

    Full Project Wipe

--- Frontend/src/app/app.component.spec.ts
+++ Frontend/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'SwordAndStone'`, () => {
+  it(`should have as title 'sword-in-stone'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('SwordAndStone');
+    expect(app.title).toEqual('sword-in-stone');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('SwordAndStone app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('sword-in-stone app is running!');
   });
 });
--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,25 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { CookieService } from 'ngx-cookie-service';  // Assuming you're using ngx-cookie-service to handle cookies
-import { AuthenticationService } from './services/authentication.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  title = "Sword and Stone";
-  username: string | null = null;
-
-  constructor(private router: Router, 
-    private cookieService: CookieService,
-    private auth: AuthenticationService) {}
-
-  ngOnInit() {
-    if (!this.auth.isLoggedIn()) {
-      console.log("REDIRECTING TO LOGIN");
-      this.router.navigate(['/login']);
-    }
-  }
+export class AppComponent {
+  title = 'sword-in-stone';
 }
